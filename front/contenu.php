3237f92c1bf1483c6980a2b9e5214a6d9b771bd4
RaSta89 <raphaelroumieux@orange.fr>
2024-11-11 15:49:52 +0100

    Création des fichiers contenu.php et styleContenu.css

---
+++ front/contenu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+    <link href="styleCSS/styleContenu.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="centrer mx-auto">
+        <p>Au Zoo Écologique Arcadia, nous vous invitons à découvrir un lieu où l'harmonie entre l'homme et la nature
+            prend
+            tout son sens.
+            Notre mission est de préserver la biodiversité, d'éduquer le public à la protection de l'environnement et
+            d'offrir aux animaux un cadre de vie aussi naturel que possible.
+            Dans notre zoo, chaque espace est conçu pour recréer les conditions de vie des animaux dans leur habitat
+            naturel.
+            De grands enclos, des espaces boisés et des zones aquatiques sont aménagés pour favoriser le bien-être des
+            espèces que nous protégeons.
+            Nous croyons que la conservation passe par la compréhension, c'est pourquoi nous nous engageons dans des
+            programmes de sensibilisation et d'éducation à la faune et à la flore.
+        </p>
+
+        <img src="../images/elephant.jpg" class="img-fluid" alt="Responsive image" alt="Bootstrap" width="1500"
+            height="366.02">
+
+        <p>À travers des expositions interactives, des animations pédagogiques et des rencontres avec des experts,
+            nous permettons à chaque visiteur de mieux comprendre les enjeux écologiques de notre époque.
+            De plus, notre engagement va au-delà de la simple exposition : nous participons activement à des projets de
+            réintroduction d'espèces menacées et soutenons
+            des initiatives locales pour préserver les écosystèmes fragiles.
+            Venez vivre une expérience immersive et enrichissante où le respect de la nature est au cœur de chaque
+            action.
+            Ensemble, protégeons notre planète et les merveilleuses créatures qui y habitent.
+        </p>
+    </div>
+</body>
