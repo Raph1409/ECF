8f13ec5a08f802f67d516628594c24d7c3e01a5a
RaSta89 <raphaelroumieux@orange.fr>
2024-11-11 11:08:45 +0100

    Création des dossier front et styleCSS, Création des fichiers header.php et styleHeader.css

---
+++ front/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+    <link href="styleCSS/styleHeader.css" rel="stylesheet">
+</head>
+
+
+<header>
+    <nav class="navbar navbar-expand-lg" style="background-color: #2B9613;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Zoo Arcadia</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/galerie">Habitats</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Connexion/Deconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
