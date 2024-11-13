87b48281e98f67897addebd42266f85df19c9739
RaSta89 <raphaelroumieux@orange.fr>
2024-11-13 16:53:55 +0100

    Création des fichiers contact.php et styleContact.css

--- front/header.php
+++ front/header.php
@@ -17,7 +17,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">Accueil</a>
+                        <a class="nav-link" href="/index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/galerie">Habitats</a>
@@ -26,7 +26,7 @@
                         <a class="nav-link" href="#">Services</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link" href="/front/contact.php">Contact</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Connexion/Deconnexion</a>
